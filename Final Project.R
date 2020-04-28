07271e5e6a1b9e35507147e198c2c761451dfb30
Vincent Pepe <vincentpepe31@gmail.com>
2020-04-28 13:20:58 -0400

    Update Final Project.R

--- Final Project.R
+++ Final Project.R
@@ -10,7 +10,9 @@ library(faraway)
 setwd("C:/Users/Vince/Documents/Vincent Work/Rutgers/data wrangling/Final Project")
 
 hockey_ref <- read.csv("hockey_reference_teams.csv") %>% 
-  drop_na() %>% 
+  tibble() %>%
+  drop_na() %>%
+  mutate(Team = as.character(Team)) %>% 
   rename("rank" = ï..Rk,
          "PTS.percent" = PTS., 
          "Simp.Rating.Sys" = SRS,
@@ -23,12 +25,21 @@ hockey_ref <- read.csv("hockey_reference_teams.csv") %>%
          "Shot.percent" = S.,
          "ShutOuts" = SO)
 
-# new_df <- hockey_ref %>% 
+# made all variables class numeric
+hockey_ref[, 3:33] <- lapply(hockey_ref[, 3:33], as.numeric)
+
+# Some tests I ran to see if removing certain variables would change anything on first dataset (still commented out as I dont want it to affect the dataset going forward)
+
+# new_df <- hockey_ref %>%
 #   select(-c(W, L, OL, PTS.percent, Team))
 # 
-# ne3w <- glm(PTS ~ ., data = new_df)  
+# tester <- hockey_ref %>% 
+#   select(-Team)
+# 
+# ne3w <- lm(PTS ~ ., data = tester)
 # summary(ne3w)
 
+# second dataset
 puckOn <- "http://www.puckon.net/" %>% 
   read_html() %>% 
   html_node("#dataTable") %>% 
@@ -47,7 +58,7 @@ puckOn <- puckOn %>%
   group_by(PTS, GP) %>% 
   arrange(-PTS, -GP)
 
-# website had incorrect number of points on it, just hardcode edited the number
+# website had incorrect number of points on it, just hardcoded the correct number
 puckOn[2,3] <- 94
 
 puckOn$rank = 1:31
@@ -68,39 +79,59 @@ puckOn <- puckOn %>%
 hockey_ref <- hockey_ref %>% 
   arrange(rank)
 
-hockey_eh <- left_join(hockey_ref, puckOn, by = c("rank", "GP", "PTS"))
+# merge to create full dataset
+hackey <- left_join(hockey_ref, puckOn, by = c("rank", "GP", "PTS"))
 
-hockey_eh <- hockey_eh %>%
+hackey <- hackey %>%
   select(-c(Team.abv, AvAge)) %>% 
   .[, -1]
 
-# hockey_standard <- hockey_eh
-#  
-# hockey_standard[, 2:43] <- scale(hockey_eh[, 2:43], center = T, scale = T)
+# *Here you can see me remove the Team variable (needs to be in model) and testing using an lm, as well as computing vif on both regular and standardized datsets*
+hackey <- hackey[, -1]
+test_lm <- lm(PTS ~., data = hackey)
+summary(test_lm)
+
+lapply(hackey, class)
+
+vif(hackey)
+
+
+hockey_standard <- hackey
+
+hockey_standard[, 2:43] <- scale(hackey[, 2:43], center = T, scale = T)
+
+vif(hockey_standard)
+
+# test and train
+hockey_standard <- hockey_standard %>% 
+  select(-Team)
+
+hockey_train.index <- hockey_standard %>%
+  initial_split()
+
+hockey.train <- training(hockey_train.index)
+hockey.test <- testing(hockey_train.index)
+
+fullmodel <- lm(PTS ~ ., data = hockey.train)
+summary(fullmodel)
+
+hybridselect <- step(fullmodel, scope = list(lower = ~ 1, upper = fullmodel),
+                     direction = "both", trace = FALSE, na.rm = T)
+summary(hybridselect)
+
+
+
+
+
+# # build model
+# (train.earth <- earth(PTS~., data = new_df))
 # 
-# hockey_train.index <- hockey_eh %>% 
-#   initial_split()
+# # estimate variable importance
+# (ev1 <- evimp (train.earth))
 # 
-# hockey.train <- training(hockey_train.index)
-# hockey.test <- testing(hockey_train.index)
-
-
-# build model
-(train.earth <- earth(PTS~., data = new_df))
-
-# estimate variable importance
-(ev1 <- evimp (train.earth))
-
-# Removing features not needed based on variable importance
+# # Removing features not needed based on variable importance
 # qb.train <- qb.train[ -c(1,3:5,7) ]
 # qb.test <- qb.test[ -c(1,3:5,7) ]
-# 
-# 
-# test <- hockey_standard[, -1]
-# 
-# whatever <- glm(PTS ~., data = test)
-# summary(whatever)
-# 
-# cor(hockey_eh$PTS, hockey_eh[,2:43], method = c("pearson"))
-# 
-# vif(whatever)
+
+
+
